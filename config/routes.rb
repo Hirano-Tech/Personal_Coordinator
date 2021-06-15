7c364dc4a3dd718e1e347799f667e8f3f5fcc349
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-06-16 01:26:20 +0900

    トップページへのルーティングを修正する。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'staff/searches#index'
+  root to: 'brand/searches#index'
+
+  namespace :brand do
+    resources :searches, only: [:index]
+  end
 
   namespace :staff do
     resources :searches, only: [:index]
