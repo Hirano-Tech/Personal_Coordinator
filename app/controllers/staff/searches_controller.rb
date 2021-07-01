f87fd74429f806894c58137b2fbc537a490f89da
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-07-01 15:31:51 +0000

    おしゃリスタ検索フォームの店舗一覧に表示させる店舗を読み込む処理を記述する。

--- app/controllers/staff/searches_controller.rb
+++ app/controllers/staff/searches_controller.rb
@@ -1,4 +1,5 @@
 class Staff::SearchesController < ApplicationController
   def index
+    @form_storelists = GuStore.readonly.select(:id, :name).find(571, 572, 605, 610, 637, 645, 667, 1242)
   end
 end
